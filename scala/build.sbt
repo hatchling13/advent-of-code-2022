9bddc122ab58a632376eff8e6408a680c0b6c691
Jeongwook Park <hatchling3713@gmail.com>
2023-07-21 22:28:50 +0900

    Completed "Day 1: Calorie Counting"

--- scala/build.sbt
+++ scala/build.sbt
@@ -1,14 +1,30 @@
 val scala3Version = "3.3.0"
 
+val zioVersion = "2.0.15"
+
+lazy val zioSettings = Seq(
+  libraryDependencies ++= Seq(
+    "dev.zio" %% "zio" % zioVersion,
+    "dev.zio" %% "zio-test" % zioVersion % Test,
+    "dev.zio" %% "zio-test-sbt" % zioVersion % Test
+  )
+)
+
 lazy val root = project
   .in(file("."))
   .settings(
     name := "scala",
     version := "0.1.0-SNAPSHOT",
-
     scalaVersion := scala3Version,
-
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    libraryDependencies ++= Seq(
+      "org.scalameta" %% "munit" % "0.7.29" % Test
+    )
   )
 
 lazy val day1 = project
+  .settings(zioSettings)
+  .settings(
+    libraryDependencies ++= Seq(
+      "com.lihaoyi" %% "os-lib" % "0.9.1"
+    )
+  )
---
+++ scala/day1/src/main/scala/Main.scala
@@ -0,0 +1,33 @@
+import zio._
+import os.Path
+
+object Main extends ZIOAppDefault {
+  val newLine = sys.props("line.separator")
+  val path = os.pwd / os.up / "inputs"
+  val inputFileNames = ZIO.attempt(os.list(path).map(_.last))
+
+  def run = for {
+    fileNames <- inputFileNames
+
+    filename <- ZIO.getOrFail(
+      fileNames.find(name => name.contains("day01.txt"))
+    )
+
+    file <- ZIO.attempt(os.read(path / filename)).catchAll(err => ZIO.fail(err))
+
+    // Refactor
+    inventories = file
+      .split(newLine * 2)
+      .toList
+      .map(inventory =>
+        inventory.split(newLine).toList.map(calory => calory.toInt)
+      )
+
+    calories = inventories.map(inventory => inventory.sum)
+
+    _ <- Console.printLine(
+      s"The Elf carrying the most Calories is carrying ${calories.max} calories!"
+    )
+
+  } yield ()
+}
